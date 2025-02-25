285e05fe2515e08019399238ba417ccb5da19ee2
JITENDRA VERMA <167751340+jitendravermaer@users.noreply.github.com>
2025-02-25 21:15:54 +0530

    Add files via upload

---
+++ SORTING/INSERTION SORT/InsertionSort.java
@@ -0,0 +1,24 @@
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        insertionSort(arr);
+        System.out.println("Sorted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
