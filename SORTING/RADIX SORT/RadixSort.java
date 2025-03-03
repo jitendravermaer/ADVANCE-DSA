bd92ae54d01078d95c8dc467b45ddb17a92b0b8e
JITENDRA VERMA <167751340+jitendravermaer@users.noreply.github.com>
2025-03-03 20:10:51 +0530

    Add files via upload

---
+++ SORTING/RADIX SORT/RadixSort.java
@@ -0,0 +1,59 @@
+public class RadixSort {
+    static void radixSort(int[] arr) {
+        int max = getMax(arr);
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countSort(arr, exp);
+        }
+    }
+
+    static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    static void countSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    static void displayArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(); // newline for readability
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
+        System.out.println("Before Sorting");
+        displayArr(arr);
+        System.out.println("After Sorting"); // newline for readability
+        radixSort(arr);
+        displayArr(arr);
+
+    }
+}
