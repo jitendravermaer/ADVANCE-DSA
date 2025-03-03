ad9d00b5ee798a16eee31afc58b125407f6ff515
JITENDRA VERMA <167751340+jitendravermaer@users.noreply.github.com>
2025-03-03 20:35:05 +0530

    Update BucketSort.java

--- SORTING/BUCKET SORT/BucketSort.java
+++ SORTING/BUCKET SORT/BucketSort.java
@@ -1,7 +1,36 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BucketSort {
     static void bucketSort(double[] arr) {
+        int n = arr.length;
+        // Buckets
+        ArrayList<Double>[] buckets = new ArrayList[n];
 
+        // Create empty buckets
+        for (int i = 0; i < n; i++) {
+            buckets[i] = new ArrayList<Double>();
+        }
+
+        // Add elements into the buckets
+        for (int i = 0; i < n; i++) {
+            int index = (int) (n * arr[i]);
+            buckets[index].add(arr[i]);
+        }
+
+        // Sort the elements of each bucket
+        for (ArrayList<Double> bucket : buckets) {
+            Collections.sort(bucket);
+        }
+
+        // Get the sorted array
+        int index = 0;
+        for (ArrayList<Double> bucket : buckets) {
+            for (int j = 0; j < bucket.size(); j++) {
+                arr[index++] = bucket.get(j);
+            }
+        }
     }
 
     static void displayArr(double[] arr) {
@@ -12,8 +41,8 @@ public class BucketSort {
     }
 
     public static void main(String[] args) {
-        double[] arr = { 0.42, 0.32, 0.33, 0.52, 0.37, 0.47, 0.51 };
-        System.out.println("Bucket Sorting:");
+        // double[] arr = { 0.42, 0.32, 0.33, 0.52, 0.37, 0.47, 0.51 };
+        double[] arr = { 0.8, 0.5, 0.6, 0.2, 0.3, 0.1, 0.4, 0.455, 0.9, 0.7, 0.89, 0.78, 0.67, 0.56, 0.45, 0.34, 0.23, 0.12, 0.11, 0.22, 0.33, 0.44, 0.55, 0.66, 0.77, 0.88, 0.99 };
 
         System.out.println("Before sorting:");
         displayArr(arr);
